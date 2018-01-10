a905a2912c76a81ca949543a2a6f5364551fe62f
James Parker <dev@jamesparker.me>
2018-01-10 00:29:31 -0500

    sign and verify test case for ed25519

--- tests/aa.rs
+++ tests/aa.rs
@@ -6,12 +6,12 @@ extern crate untrusted;
 
 use crypto_abstract::ToPublicKey;
 use crypto_abstract::asym::auth;
-use ring::rand::SystemRandom;
+use ring::rand::{SystemRandom, SecureRandom};
 use ring::signature::Ed25519KeyPair;
 use untrusted::Input;
 
 #[test] 
-fn to_public_key_test() {
+fn ed25519_to_public_key_test() {
     fn run() {
         // Generate key.
         let rng = SystemRandom::new();
@@ -27,6 +27,32 @@ fn to_public_key_test() {
 
         // Ensure outputs match.
         assert_eq!( pkak, rk);
+    }
+
+    for _ in 1..100 {
+        run()
+    }
+}
+
+#[test]
+fn ed25519_sign_and_verify() {
+    fn run() {
+        // Generate key.
+        let rng = SystemRandom::new();
+        let key = auth::gen( &rng, &auth::Algorithm::AAEd25519).unwrap();
+
+        // Generate something to sign.
+        let mut content = [0u8; 256].to_vec();
+        rng.fill( &mut content).unwrap();
+
+        // Sign content.
+        let signed = auth::sign( &key, &content).unwrap();
+
+        // Verify content.
+        let public_key = ToPublicKey::to_public_key( &key);
+        let valid = auth::verify( &public_key, &content, &signed);
+
+        assert!( valid);
     }
 
     for _ in 1..100 {
